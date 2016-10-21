7caaf895f91e1f0a42732ba55e7f0a341a611120
Wchabh <519381438@qq.com>
2016-10-21 08:36:54 +0800

    alpha 1.1

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from PyQt5 import QtWidgets
+import PyQt5
 from login import *
 from PyQt5.QtWidgets import QApplication , QMainWindow
-class Glob(object):
-    "This is a place to save some message"
+import Globvar
 class mywindow(QMainWindow,Ui_MainWindow):
-    def showtext(self):
-        Glob.Username=self.lineEdit.text()
-        Glob.Password=self.lineEdit_2.text()
-        print(Glob.Username,Glob.Password)
+    def GetUserName(self):
+        Username=self.lineEdit.text()
+        Password=self.lineEdit_2.text()
+        _Glob.receiveUsername(Username,Password)
+    def ok(self):
+        print("OK")
     def __init__(self):
         self._UserNameHaveChanged=0
         self._PassWordHaveChanged=0
         super(mywindow,self).__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.showtext)
+        self.pushButton.clicked.connect(self.GetUserName)
+        self.lineEdit.setAttribute(QtCore.Qt.WA_InputMethodEnabled, False)
+        self.lineEdit_2.setAttribute(QtCore.Qt.WA_InputMethodEnabled, False)
 if __name__=="__main__":
     import sys
     app=QtWidgets.QApplication(sys.argv)
+    _Glob=Globvar.Glob()
     myshow=mywindow()
     myshow.show()
     sys.exit(app.exec_())
