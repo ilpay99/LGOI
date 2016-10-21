96ac6b88dde59a8575944f066a92c28f032522b9
Wchabh <519381438@qq.com>
2016-10-21 09:22:38 +0800

    additon 1

---
+++ Globvar.py
@@ -0,0 +1,8 @@
+class Glob(object):
+	"""This is a place to save some important data"""
+	def receiveUsername(self,Username,Password):
+		self._Password=Password
+		self._Username=Username
+	def __init__(self):
+		pass
+a=Glob()
